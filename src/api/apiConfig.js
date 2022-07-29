e5e9066bd313b95ee39b671b36aa4a78e818c028
nguyencnhattan <nguyencnhattan@gmail.com>
2022-07-30 02:19:17 +0700

    Make HeroSlide

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,5 +1,5 @@
 const apiConfig = {
-    baseUrl: 'http://api.themovieb.org/3/',
+    baseUrl: 'https://api.themoviedb.org/3/',
     apiKey: 'f6c0b7382cd424f48fee527e28af8b89',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
